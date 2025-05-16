77093d7bd8361be278b55495e06c941043ab93ac
Gabriel Menezes <130420155+Gaaaybe@users.noreply.github.com>
2025-05-16 19:26:20 -0300

    feat: add Redis configuration to environment variables

--- src/utils/redisConnection.ts
+++ src/utils/redisConnection.ts
@@ -1,11 +1,12 @@
 import { QueueOptions } from "bullmq";
+import { config } from "dotenv";
 
 export const bullMQConnection: QueueOptions = {
     connection: {
-        host: "localhost",
-        port: 6379,
-        password: undefined,
-        db: 0,
+        host: process.env.REDIS_HOST || "localhost",
+        port: Number(process.env.REDIS_PORT) || 6379,
+        password: process.env.REDIS_PASSWORD || "",
+        db: Number(process.env.REDIS_DB) || 0,
     }
 };
 
